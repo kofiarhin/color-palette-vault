415cfb2fc46e2a61d18e7678d501b4e52256ad88
kofi arhin <kofiarhin69@gmail.com>
2025-07-31 16:20:15 +0100

    init

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import "./app.styles.scss";
+
+const App = () => {
+  return (
+    <div id="app">
+      <h1 className="heading">Color Palette Vault</h1>
+      <p>Watch This Space</p>
+      <p>
+        Follow on instagram{" "}
+        <a href="https://www.instagram.com/colorpalettevault/" target="_blank">
+          Click Here
+        </a>{" "}
+      </p>
+    </div>
+  );
+};
+
+export default App;
